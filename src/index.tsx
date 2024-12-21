2a43e765716314927a3abfaed590ac066fc2eda2
blck-snwmn <whitesnowmancreator@gmail.com>
2024-12-21 20:17:43 +0900

    Update layout

--- src/index.tsx
+++ src/index.tsx
@@ -43,8 +43,8 @@ export default {
 				image_url={meta.result.ogImage?.[0].url}
 			/>,
 			{
-				width: 1200,
-				height: 630,
+				width: 600,
+				height: 800,
 				fonts: [
 					{
 						name: "Roboto",
@@ -102,61 +102,23 @@ const OGPCard: React.FC<ComponentProps> = ({ title, description, image_url }) =>
 		<div
 			style={{
 				display: "flex",
-				flexDirection: "row",
-				width: "1200px",
-				height: "630px",
+				flexDirection: "column",
+				width: "600px",
+				height: "800px",
 				backgroundColor: "#ADD8E6",
+				border: "5px solid #00bfff", // 目立つ枠
 				overflow: "hidden",
 			}}
 		>
-			{/* Left Section: Title and Description */}
+			{/* Top Section: Image */}
 			<div
 				style={{
-					flex: 1,
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					padding: "40px",
-					overflow: "hidden",
-				}}
-			>
-				<h1
-					style={{
-						fontSize: "36px",
-						fontWeight: "bold",
-						marginBottom: "16px",
-						overflow: "hidden",
-						display: "-webkit-box",
-						WebkitLineClamp: 2, // 最大2行まで
-						WebkitBoxOrient: "vertical",
-						whiteSpace: "normal", // 折り返しを許可
-					}}
-				>
-					{title || "Default Title"}
-				</h1>
-				<p
-					style={{
-						fontSize: "24px",
-						color: "#555",
-						overflow: "hidden",
-						display: "-webkit-box",
-						WebkitLineClamp: 3, // 最大3行まで
-						WebkitBoxOrient: "vertical",
-						whiteSpace: "normal", // 折り返しを許可
-					}}
-				>
-					{description || "Default description text that can wrap around to multiple lines if necessary."}
-				</p>
-			</div>
-
-			{/* Right Section: Image */}
-			<div
-				style={{
-					width: "600px",
-					height: "100%",
+					width: "100%",
+					height: "315px", // 高さを半分に設定
 					display: "flex",
 					alignItems: "center",
 					justifyContent: "center",
+					overflow: "hidden",
 				}}
 			>
 				{image_url ? (
@@ -166,7 +128,7 @@ const OGPCard: React.FC<ComponentProps> = ({ title, description, image_url }) =>
 						style={{
 							width: "100%",
 							height: "100%",
-							objectFit: "contain", // 見切れ防止
+							objectFit: "cover", // 見切れ防止
 						}}
 					/>
 				) : (
@@ -185,6 +147,48 @@ const OGPCard: React.FC<ComponentProps> = ({ title, description, image_url }) =>
 						No Image
 					</div>
 				)}
+			</div>
+
+			{/* Bottom Section: Title and Description */}
+			<div
+				style={{
+					flex: 1,
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+					padding: "20px",
+					overflow: "hidden",
+				}}
+			>
+				<h1
+					style={{
+						fontSize: "36px",
+						fontWeight: "bold",
+						marginBottom: "16px",
+						height: "72px", // 高さを固定
+						overflow: "hidden",
+						display: "-webkit-box",
+						WebkitLineClamp: 2, // 最大2行まで
+						WebkitBoxOrient: "vertical",
+						whiteSpace: "normal", // 折り返しを許可
+					}}
+				>
+					{title || "Default Title"}
+				</h1>
+				<p
+					style={{
+						fontSize: "24px",
+						color: "#555",
+						height: "72px", // 高さを固定
+						overflow: "hidden",
+						display: "-webkit-box",
+						WebkitLineClamp: 3, // 最大3行まで
+						WebkitBoxOrient: "vertical",
+						whiteSpace: "normal", // 折り返しを許可
+					}}
+				>
+					{description || "Default description text that can wrap around to multiple lines if necessary."}
+				</p>
 			</div>
 		</div>
 	);
